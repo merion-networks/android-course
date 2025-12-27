7d6bbd22edfc81fff4e41fb114fa624671c17d3e
Nikolai Kotchetkov <motorro@gmail.com>
2025-12-27 03:11:33 +0100

    Core libs

---
+++ core/src/main/kotlin/com/motorro/core/log/log.kt
@@ -0,0 +1,22 @@
+package com.motorro.core.log
+
+import io.github.aakira.napier.LogLevel
+import io.github.aakira.napier.Napier
+
+interface Logging {
+    
+    val tag: String get() = javaClass.simpleName
+
+    fun d(throwable: Throwable? = null, message: () -> String) {
+        Napier.log(LogLevel.DEBUG, tag, throwable, message())
+    }
+    fun i(throwable: Throwable? = null, message: () -> String) {
+        Napier.log(LogLevel.INFO, tag, throwable, message())
+    }
+    fun w(throwable: Throwable? = null, message: () -> String) {
+        Napier.log(LogLevel.WARNING, tag, throwable, message())
+    }
+    fun e(throwable: Throwable? = null, message: () -> String) {
+        Napier.log(LogLevel.ERROR, tag, throwable, message())
+    }
+}
