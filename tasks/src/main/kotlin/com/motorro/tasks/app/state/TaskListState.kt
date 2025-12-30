motorro/Android

Task completion toggles Refs: #4

tasks/src/main/kotlin/ru/merionet/tasks/app/state/TaskListState.kt
@@ -1,7 +1,19 @@
 package ru.merionet.tasks.app.state
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.consumeAsFlow
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.mapNotNull
 import kotlinx.coroutines.flow.onEach
+import ru.merionet.core.coroutines.DispatcherProvider
 import ru.merionet.core.lce.LceState
 import ru.merionet.tasks.app.data.AppData
 import ru.merionet.tasks.app.data.AppError
@@ -9,6 +21,7 @@ import ru.merionet.tasks.app.data.AppGesture
 import ru.merionet.tasks.app.data.AppUiState
 import ru.merionet.tasks.app.repository.TasksRepository
 import ru.merionet.tasks.data.Task
+import ru.merionet.tasks.data.TaskId
 import javax.inject.Inject
 
 /**
@@ -17,11 +30,15 @@ import javax.inject.Inject
 class TaskListState(
     context: AppContext,
     private val data: AppData,
-    private val tasksRepository: TasksRepository
+    private val tasksRepository: TasksRepository,
+    private val dispatchers: DispatcherProvider
 ) : BaseAppState(context) {
 
     private var tasks: Collection<Task> = emptyList()
     private var updateStatus: LceState<Unit, AppError> = LceState.Content(Unit)
+
+    private val tasksChannel = Channel<Collection<Task>>(capacity = Channel.CONFLATED)
+    private val toggles = MutableSharedFlow<TaskId>(extraBufferCapacity = 1, onBufferOverflow = BufferOverflow.DROP_OLDEST)
 
     /**
      * Combined loading status
@@ -46,6 +63,7 @@ class TaskListState(
     override fun doStart() {
         super.doStart()
         subscribeTasks()
+        subscribeToggles()
         update()
     }
 
@@ -55,7 +73,9 @@ class TaskListState(
     private fun subscribeTasks() {
         tasksRepository.getTasks(data.user.name)
             .onEach { tasks = it }
+            .onEach { tasksChannel.send(it) }
             .onEach { render() }
+            .flowOn(Dispatchers.Default)
             .launchIn(stateScope)
     }
 
@@ -81,6 +101,30 @@ class TaskListState(
     }
 
     /**
+     * Subscribes toggles and schedules task updates on each click
+     * Consider some flow debounce or update batching in real world app
+     */
+    @OptIn(ExperimentalCoroutinesApi::class)
+    private fun subscribeToggles() {
+        tasksChannel.consumeAsFlow()
+            .map { it.associateBy { task -> task.id } }
+            .flatMapLatest { taskMap ->
+                toggles
+                    .mapNotNull { taskMap[it] }
+                    .map { it.copy(complete = it.complete.not()) }
+                    .distinctUntilChanged { old, new -> old.complete != new.complete }
+                    .onEach { update ->
+                        tasksRepository.upsertTaskAsync(
+                            data.user.name,
+                            update
+                        )
+                    }
+            }
+            .flowOn(dispatchers.default)
+            .launchIn(stateScope)
+    }
+
+    /**
      * A part of [process] template to process UI gesture
      */
     override fun doProcess(gesture: AppGesture) {
@@ -93,6 +137,9 @@ class TaskListState(
                 val task = tasks.find { gesture.id == it.id } ?: return
                 d { "Task clicked: ${gesture.id}" }
                 setMachineState(factory.task(data, task))
+            }
+            is AppGesture.TaskList.TaskToggled -> {
+                toggles.tryEmit(gesture.id)
             }
             AppGesture.Refresh -> {
                 d { "Refresh gesture" }
@@ -150,11 +197,15 @@ class TaskListState(
      * State factory to inject all the external dependencies and keep the main factory
      * clean
      */
-    class Factory @Inject constructor(private val tasksRepository: TasksRepository) {
+    class Factory @Inject constructor(
+        private val tasksRepository: TasksRepository,
+        private val dispatchers: DispatcherProvider
+    ) {
         operator fun invoke(context: AppContext, data: AppData) = TaskListState(
             context,
             data,
-            tasksRepository
+            tasksRepository,
+            dispatchers
         )
     }
 }

tasks/src/test/kotlin/ru/merionet/tasks/app/state/TaskListStateTest.kt
@@ -1,8 +1,11 @@
 package ru.merionet.tasks.app.state
 
 import io.mockk.Ordering
+import io.mockk.Runs
+import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.every
+import io.mockk.just
 import io.mockk.mockk
 import io.mockk.verify
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -11,6 +14,7 @@ import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.test.runTest
 import org.junit.Test
 import ru.merionet.core.lce.LceState
+import ru.merionet.tasks.TestDispatchers
 import ru.merionet.tasks.USER_NAME
 import ru.merionet.tasks.app.data.AppError
 import ru.merionet.tasks.app.data.AppGesture
@@ -24,7 +28,12 @@ import java.io.IOException
 
 internal class TaskListStateTest : BaseStateTest() {
     private val tasksRepository: TasksRepository = mockk()
-    private val state = TaskListState(context, appData, tasksRepository)
+    private val state = TaskListState(
+        context,
+        appData,
+        tasksRepository,
+        TestDispatchers(dispatcher)
+    )
 
     @Test
     fun subscribesAndUpdatesTasksOnStart() = runTest(dispatcher) {
@@ -238,6 +247,26 @@ internal class TaskListStateTest : BaseStateTest() {
         verify(ordering = Ordering.ORDERED) {
             factory.task(appData, task1)
             stateMachine.setMachineState(nextState)
+        }
+    }
+
+    @Test
+    fun completesTask() = runTest {
+        every { tasksRepository.getTasks(any()) } returns flowOf(listOf(task1, task2)).stateIn(this)
+        every { tasksRepository.update(any()) } returns flowOf(
+            LceState.Loading(),
+            LceState.Content(Unit)
+        )
+        coEvery { tasksRepository.upsertTaskAsync(any(), any()) } just Runs
+
+        state.start(stateMachine)
+        state.process(AppGesture.TaskList.TaskToggled(task1.id))
+
+        coVerify(Ordering.ORDERED) {
+            tasksRepository.upsertTaskAsync(
+                USER_NAME,
+                task1.copy(complete = true)
+            )
         }
     }
 

merion-networks/android-course

(cherry picked from commit 9fbb5273def37fe2e264368e21f81ccd303ff77f)

tasks/src/main/kotlin/com/motorro/tasks/app/state/TaskListState.kt
@@ -1,5 +1,6 @@
 package com.motorro.tasks.app.state
 
+import com.motorro.core.coroutines.DispatcherProvider
 import com.motorro.core.lce.LceState
 import com.motorro.tasks.app.data.AppData
 import com.motorro.tasks.app.data.AppError
@@ -7,8 +8,21 @@ import com.motorro.tasks.app.data.AppGesture
 import com.motorro.tasks.app.data.AppUiState
 import com.motorro.tasks.app.repository.TasksRepository
 import com.motorro.tasks.data.Task
+import com.motorro.tasks.data.TaskId
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.consumeAsFlow
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.mapNotNull
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 /**
@@ -17,11 +31,15 @@ import javax.inject.Inject
 class TaskListState(
     context: AppContext,
     private val data: AppData,
-    private val tasksRepository: TasksRepository
+    private val tasksRepository: TasksRepository,
+    private val dispatchers: DispatcherProvider
 ) : BaseAppState(context) {
 
     private var tasks: Collection<Task> = emptyList()
     private var updateStatus: LceState<Unit, AppError> = LceState.Content(Unit)
+
+    private val tasksChannel = Channel<Collection<Task>>(capacity = Channel.CONFLATED)
+    private val toggles = MutableSharedFlow<TaskId>(extraBufferCapacity = 1, onBufferOverflow = BufferOverflow.DROP_OLDEST)
 
     /**
      * Combined loading status
@@ -46,6 +64,7 @@ class TaskListState(
     override fun doStart() {
         super.doStart()
         subscribeTasks()
+        subscribeToggles()
         update()
     }
 
@@ -55,7 +74,9 @@ class TaskListState(
     private fun subscribeTasks() {
         tasksRepository.getTasks(data.user.name)
             .onEach { tasks = it }
+            .onEach { tasksChannel.send(it) }
             .onEach { render() }
+            .flowOn(Dispatchers.Default)
             .launchIn(stateScope)
     }
 
@@ -81,6 +102,30 @@ class TaskListState(
     }
 
     /**
+     * Subscribes toggles and schedules task updates on each click
+     * Consider some flow debounce or update batching in real world app
+     */
+    @OptIn(ExperimentalCoroutinesApi::class)
+    private fun subscribeToggles() {
+        tasksChannel.consumeAsFlow()
+            .map { it.associateBy { task -> task.id } }
+            .flatMapLatest { taskMap ->
+                toggles
+                    .mapNotNull { taskMap[it] }
+                    .map { it.copy(complete = it.complete.not()) }
+                    .distinctUntilChanged { old, new -> old.complete != new.complete }
+                    .onEach { update ->
+                        tasksRepository.upsertTaskAsync(
+                            data.user.name,
+                            update
+                        )
+                    }
+            }
+            .flowOn(dispatchers.default)
+            .launchIn(stateScope)
+    }
+
+    /**
      * A part of [process] template to process UI gesture
      */
     override fun doProcess(gesture: AppGesture) {
@@ -93,6 +138,9 @@ class TaskListState(
                 val task = tasks.find { gesture.id == it.id } ?: return
                 d { "Task clicked: ${gesture.id}" }
                 setMachineState(factory.task(data, task))
+            }
+            is AppGesture.TaskList.TaskToggled -> stateScope.launch {
+                toggles.emit(gesture.id)
             }
             AppGesture.Refresh -> {
                 d { "Refresh gesture" }
@@ -150,11 +198,15 @@ class TaskListState(
      * State factory to inject all the external dependencies and keep the main factory
      * clean
      */
-    class Factory @Inject constructor(private val tasksRepository: TasksRepository) {
+    class Factory @Inject constructor(
+        private val tasksRepository: TasksRepository,
+        private val dispatchers: DispatcherProvider
+    ) {
         operator fun invoke(context: AppContext, data: AppData) = TaskListState(
             context,
             data,
-            tasksRepository
+            tasksRepository,
+            dispatchers
         )
     }
 }

tasks/src/test/kotlin/com/motorro/tasks/app/state/TaskListStateTest.kt
@@ -1,6 +1,7 @@
 package com.motorro.tasks.app.state
 
 import com.motorro.core.lce.LceState
+import com.motorro.tasks.TestDispatchers
 import com.motorro.tasks.USER_NAME
 import com.motorro.tasks.app.data.AppError
 import com.motorro.tasks.app.data.AppGesture
@@ -11,8 +12,11 @@ import com.motorro.tasks.app.task2
 import com.motorro.tasks.auth.data.SessionError
 import com.motorro.tasks.data.ErrorCode
 import io.mockk.Ordering
+import io.mockk.Runs
+import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.every
+import io.mockk.just
 import io.mockk.mockk
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.flowOf
@@ -23,7 +27,12 @@ import java.io.IOException
 
 internal class TaskListStateTest : BaseStateTest() {
     private val tasksRepository: TasksRepository = mockk()
-    private val state = TaskListState(context, appData, tasksRepository)
+    private val state = TaskListState(
+        context,
+        appData,
+        tasksRepository,
+        TestDispatchers(dispatcher)
+    )
 
     @Test
     @Suppress("UnusedFlow")
@@ -243,6 +252,26 @@ internal class TaskListStateTest : BaseStateTest() {
         coVerify {
             factory.task(appData, task1)
             stateMachine.setMachineState(nextState)
+        }
+    }
+
+    @Test
+    fun completesTask() = runTest {
+        every { tasksRepository.getTasks(any()) } returns flowOf(listOf(task1, task2)).stateIn(this)
+        every { tasksRepository.update(any()) } returns flowOf(
+            LceState.Loading(),
+            LceState.Content(Unit)
+        )
+        coEvery { tasksRepository.upsertTaskAsync(any(), any()) } just Runs
+
+        state.start(stateMachine)
+        state.process(AppGesture.TaskList.TaskToggled(task1.id))
+
+        coVerify {
+            tasksRepository.upsertTaskAsync(
+                USER_NAME,
+                task1.copy(complete = true)
+            )
         }
     }
 