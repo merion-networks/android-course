motorro/Android

Server updates in tasks repository Refs: #4

tasks/src/main/kotlin/ru/merionet/tasks/app/data/AppError.kt
@@ -6,6 +6,7 @@ import androidx.compose.ui.platform.LocalContext
 import ru.merionet.core.error.WithRetry
 import ru.merionet.tasks.R
 import ru.merionet.tasks.auth.data.SessionError
+import ru.merionet.tasks.data.ErrorCode
 
 /**
  * Application error
@@ -29,6 +30,16 @@ sealed class AppError : Exception(), WithRetry {
          * No point in retrying before user changes the input
          */
         override val retriable: Boolean = cause.retriable
+    }
+
+    /**
+     * Flow error. Session data conflict, illegal state, etc
+     */
+    class WorkFlow(val code: ErrorCode, override val message: String) : AppError() {
+        /**
+         * No point in retrying before altering the flow
+         */
+        override val retriable: Boolean = false
     }
 
     /**
@@ -67,6 +78,7 @@ fun AppError.getMessage(context: Context): String {
     val originalMessage = message ?: cause?.message
     return when (this) {
         is AppError.Authentication -> context.getString(R.string.err_authentication, originalMessage ?: context.getString(R.string.err_unknown))
+        is AppError.WorkFlow -> context.getString(R.string.err_workflow, originalMessage ?: context.getString(R.string.err_unknown))
         is AppError.Network -> context.getString(R.string.err_network)
         is AppError.Storage -> context.getString(R.string.err_storage)
         is AppError.Unknown -> originalMessage ?: context.getString(R.string.err_unknown)

tasks/src/main/kotlin/ru/merionet/tasks/app/repository/TasksRepository.kt
@@ -10,10 +10,15 @@ import ru.merionet.tasks.app.data.AppError
 import ru.merionet.tasks.app.data.toApp
 import ru.merionet.tasks.app.net.TasksApi
 import ru.merionet.tasks.app.net.netResult
+import ru.merionet.tasks.data.ErrorCode
 import ru.merionet.tasks.data.Task
+import ru.merionet.tasks.data.TaskCommand
 import ru.merionet.tasks.data.TaskId
+import ru.merionet.tasks.data.TaskUpdateRequest
+import ru.merionet.tasks.data.TaskUpdates
 import ru.merionet.tasks.data.UserName
 import ru.merionet.tasks.data.Version
+import ru.merionet.tasks.data.nextVersion
 import javax.inject.Inject
 
 /**
@@ -37,14 +42,14 @@ interface TasksRepository : ReadonlyTasks {
      * @param userName Bound user
      * @param task Task to update
      */
-    fun upsertTask(userName: UserName, task: Task): Flow<LceState<Unit, AppError>>
+    suspend fun upsertTask(userName: UserName, task: Task)
 
     /**
      * Delete task
      * @param userName Bound user
      * @param taskId Task ID
      */
-    fun deleteTask(userName: UserName, taskId: TaskId): Flow<LceState<Unit, AppError>>
+    suspend fun deleteTask(userName: UserName, taskId: TaskId)
 
     /**
      * Repository implementation (memory caching)
@@ -75,30 +80,84 @@ interface TasksRepository : ReadonlyTasks {
         private fun load(userName: UserName, version: Version?): Flow<LceState<Unit, AppError>> = flow {
             emit(LceState.Loading(Unit))
 
-            d { "Updating task list..." }
-            val changes = netResult { tasksApi.getUpdates(version) }.getOrElse {
+            doLoad(userName, version).getOrElse {
                 emit(LceState.Error(it.toApp(), Unit))
                 return@flow
             }
-
-            storage.update(userName, changes)
 
             emit(LceState.Content(Unit))
         }
 
         /**
+         * Does the update
+         */
+        private suspend fun doLoad(userName: UserName, version: Version?): Result<Version> {
+            d { "Updating task list..." }
+            return netResult { tasksApi.getUpdates(version) }.mapCatching { changes -> storage.update(userName, changes) }
+        }
+
+        /**
          * Insert / update
          */
-        override fun upsertTask(userName: UserName, task: Task): Flow<LceState<Unit, AppError>> {
-            TODO("Not yet implemented")
+        override suspend fun upsertTask(userName: UserName, task: Task) {
+            d { "Upserting task: $task" }
+            updateServer(userName, listOf(TaskCommand.Upsert(task)))
         }
 
         /**
          * Delete task
          * @param taskId Task ID
          */
-        override fun deleteTask(userName: UserName, taskId: TaskId): Flow<LceState<Unit, AppError>> {
-            TODO("Not yet implemented")
+        override suspend fun deleteTask(userName: UserName, taskId: TaskId) {
+            d { "Deleting task: $taskId" }
+            updateServer(userName, listOf(TaskCommand.Delete(taskId)))
+        }
+
+        /**
+         * Runs update from server
+         * @param userName Bound user
+         * @param commands Commands to run
+         * @param currentVersion Current version override
+         */
+        private suspend fun updateServer(
+            userName: UserName,
+            commands: List<TaskCommand>,
+            currentVersion: Version? = null
+        ) {
+            // Loads data and retries the update
+            suspend fun syncAndRetry(version: Version?) {
+                updateServer(
+                    userName,
+                    commands,
+                    doLoad(userName, version).getOrThrow()
+                )
+            }
+
+            val effectiveCurrentVersion = currentVersion ?: storage.getVersion(userName).firstOrNull()
+            if (null == effectiveCurrentVersion) {
+                d { "Empty version. Updating data..." }
+                syncAndRetry(null)
+                return
+            }
+
+            val newVersion = nextVersion()
+            d { "Updating server" }
+            d { "Current version: $currentVersion" }
+            d { "New version: $newVersion" }
+
+            val update = netResult { tasksApi.postUpdates(TaskUpdateRequest(effectiveCurrentVersion, newVersion, commands)) }
+                .recover { error ->
+                    w(error) { "Error updating tasks" }
+                    if (error is AppError.WorkFlow && ErrorCode.CONFLICT == error.code) {
+                        d { "Data conflict. Retrying after data sync" }
+                        syncAndRetry(effectiveCurrentVersion)
+                        return
+                    }
+                    throw error
+                }
+                .getOrThrow()
+
+            storage.update(userName, TaskUpdates(update.latestVersion, commands))
         }
     }
 }

tasks/src/test/kotlin/ru/merionet/tasks/app/repository/MemoryTasksRepositoryTest.kt
@@ -1,11 +1,14 @@
 package ru.merionet.tasks.app.repository
 
+import io.mockk.Ordering
 import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.cancelAndJoin
+import kotlinx.coroutines.flow.take
 import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.joinAll
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.runTest
@@ -22,12 +25,15 @@ import ru.merionet.tasks.data.ErrorCode
 import ru.merionet.tasks.data.HttpResponse
 import ru.merionet.tasks.data.Task
 import ru.merionet.tasks.data.TaskCommand
+import ru.merionet.tasks.data.TaskUpdateRequest
 import ru.merionet.tasks.data.TaskUpdates
 import ru.merionet.tasks.data.Version
+import ru.merionet.tasks.data.VersionResponse
 import java.util.LinkedList
 import kotlin.test.assertContentEquals
 import kotlin.test.assertEquals
 import kotlin.test.assertIs
+import kotlin.test.assertNotEquals
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class MemoryTasksRepositoryTest {
@@ -114,6 +120,7 @@ class MemoryTasksRepositoryTest {
             tasks.map { it.toList() }
         )
         vCollector.cancelAndJoin()
+        @Suppress("RemoveExplicitTypeArguments")
         assertEquals<List<Version?>>(
             listOf(null),
             versions
@@ -121,6 +128,112 @@ class MemoryTasksRepositoryTest {
 
         coVerify {
             api.getUpdates(null)
+        }
+    }
+
+    @Test
+    fun upsertsTask() = runTest(dispatcher) {
+        val repository = createRepository()
+        val ver1 = Version("ver1")
+        coEvery { api.getUpdates(anyNullable()) } returns HttpResponse.Data(
+            TaskUpdates(
+                ver1,
+                listOf(task1).map(TaskCommand::Upsert)
+            )
+        )
+        coEvery { api.postUpdates(any()) } answers {
+            HttpResponse.Data(VersionResponse((args.first() as TaskUpdateRequest).nextVersion))
+        }
+
+        val tasks = LinkedList<Collection<Task>>()
+        val tCollector = launch {
+            repository.getTasks(USER_NAME).take(3).collect(tasks::add)
+        }
+
+        val versions = LinkedList<Version?>()
+        val vCollector = launch {
+            repository.getVersion(USER_NAME).take(3).collect(versions::add)
+        }
+
+        repository.upsertTask(USER_NAME, task2)
+
+        joinAll(tCollector, vCollector)
+        assertEquals(
+            listOf(emptyList(), listOf(task1), listOf(task1, task2)),
+            tasks.map { it.toList() }
+        )
+        assertEquals(3, versions.size)
+
+        coVerify {
+            api.getUpdates(null)
+            api.postUpdates(withArg {
+                assertEquals(ver1, it.expectedVersion)
+                assertNotEquals(ver1, it.nextVersion)
+                assertEquals(listOf(TaskCommand.Upsert(task2)), it.commands)
+            })
+        }
+    }
+
+    @Test
+    fun syncsAndRetriesOnVersionConflict() = runTest(dispatcher) {
+        val repository = createRepository()
+        val ver1 = Version("ver1")
+        val ver2 = Version("ver2")
+        coEvery { api.getUpdates(anyNullable()) }.returnsMany(
+            HttpResponse.Data(
+                TaskUpdates(
+                    ver1,
+                    listOf(task1).map(TaskCommand::Upsert)
+                )
+            ),
+            HttpResponse.Data(
+                TaskUpdates(
+                    ver2,
+                    listOf(task1.id).map(TaskCommand::Delete)
+                )
+            )
+        )
+        var index = 1
+        coEvery { api.postUpdates(any()) } answers {
+            when(index++) {
+                1 -> HttpResponse.Error(ErrorCode.CONFLICT, "Version conflict")
+                2 -> HttpResponse.Data(VersionResponse((args.first() as TaskUpdateRequest).nextVersion))
+                else -> throw IllegalStateException("Unexpected invocation")
+            }
+        }
+
+        val tasks = LinkedList<Collection<Task>>()
+        val tCollector = launch {
+            repository.getTasks(USER_NAME).take(4).collect(tasks::add)
+        }
+
+        val versions = LinkedList<Version?>()
+        val vCollector = launch {
+            repository.getVersion(USER_NAME).take(4).collect(versions::add)
+        }
+
+        repository.upsertTask(USER_NAME, task2)
+
+        joinAll(tCollector, vCollector)
+        assertEquals(
+            listOf(emptyList(), listOf(task1), emptyList(), listOf(task2)),
+            tasks.map { it.toList() }
+        )
+        assertEquals(4, versions.size)
+
+        coVerify(ordering = Ordering.SEQUENCE) {
+            api.getUpdates(null)
+            api.postUpdates(withArg {
+                assertEquals(ver1, it.expectedVersion)
+                assertNotEquals(ver1, it.nextVersion)
+                assertEquals(listOf(TaskCommand.Upsert(task2)), it.commands)
+            })
+            api.getUpdates(ver1)
+            api.postUpdates(withArg {
+                assertEquals(ver2, it.expectedVersion)
+                assertNotEquals(ver1, it.nextVersion)
+                assertEquals(listOf(TaskCommand.Upsert(task2)), it.commands)
+            })
         }
     }
 }

merion-networks/android-course

(cherry picked from commit 7916e3fd4ca20a3b1029933a2f2e69156296bdd7)

tasks/src/main/kotlin/com/motorro/tasks/app/data/AppError.kt
@@ -6,6 +6,7 @@ import androidx.compose.ui.platform.LocalContext
 import com.motorro.core.error.WithRetry
 import com.motorro.tasks.R
 import com.motorro.tasks.auth.data.SessionError
+import com.motorro.tasks.data.ErrorCode
 
 /**
  * Application error
@@ -29,6 +30,16 @@ sealed class AppError : Exception(), WithRetry {
          * No point in retrying before user changes the input
          */
         override val retriable: Boolean = cause.retriable
+    }
+
+    /**
+     * Flow error. Session data conflict, illegal state, etc
+     */
+    class WorkFlow(val code: ErrorCode, override val message: String) : AppError() {
+        /**
+         * No point in retrying before altering the flow
+         */
+        override val retriable: Boolean = false
     }
 
     /**
@@ -67,6 +78,7 @@ fun AppError.getMessage(context: Context): String {
     val originalMessage = message ?: cause?.message
     return when (this) {
         is AppError.Authentication -> context.getString(R.string.err_authentication, originalMessage ?: context.getString(R.string.err_unknown))
+        is AppError.WorkFlow -> context.getString(R.string.err_workflow, originalMessage ?: context.getString(R.string.err_unknown))
         is AppError.Network -> context.getString(R.string.err_network)
         is AppError.Storage -> context.getString(R.string.err_storage)
         is AppError.Unknown -> originalMessage ?: context.getString(R.string.err_unknown)

tasks/src/main/kotlin/com/motorro/tasks/app/repository/TasksRepository.kt
@@ -6,10 +6,15 @@ import com.motorro.tasks.app.data.AppError
 import com.motorro.tasks.app.data.toApp
 import com.motorro.tasks.app.net.TasksApi
 import com.motorro.tasks.app.net.netResult
+import com.motorro.tasks.data.ErrorCode
 import com.motorro.tasks.data.Task
+import com.motorro.tasks.data.TaskCommand
 import com.motorro.tasks.data.TaskId
+import com.motorro.tasks.data.TaskUpdateRequest
+import com.motorro.tasks.data.TaskUpdates
 import com.motorro.tasks.data.UserName
 import com.motorro.tasks.data.Version
+import com.motorro.tasks.data.nextVersion
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.emitAll
 import kotlinx.coroutines.flow.firstOrNull
@@ -37,14 +42,14 @@ interface TasksRepository : ReadonlyTasks {
      * @param userName Bound user
      * @param task Task to update
      */
-    fun upsertTask(userName: UserName, task: Task): Flow<LceState<Unit, AppError>>
+    suspend fun upsertTask(userName: UserName, task: Task)
 
     /**
      * Delete task
      * @param userName Bound user
      * @param taskId Task ID
      */
-    fun deleteTask(userName: UserName, taskId: TaskId): Flow<LceState<Unit, AppError>>
+    suspend fun deleteTask(userName: UserName, taskId: TaskId)
 
     /**
      * Repository implementation (memory caching)
@@ -75,30 +80,84 @@ interface TasksRepository : ReadonlyTasks {
         private fun load(userName: UserName, version: Version?): Flow<LceState<Unit, AppError>> = flow {
             emit(LceState.Loading(Unit))
 
-            d { "Updating task list..." }
-            val changes = netResult { tasksApi.getUpdates(version) }.getOrElse {
+            doLoad(userName, version).getOrElse {
                 emit(LceState.Error(it.toApp(), Unit))
                 return@flow
             }
-
-            storage.update(userName, changes)
 
             emit(LceState.Content(Unit))
         }
 
         /**
+         * Does the update
+         */
+        private suspend fun doLoad(userName: UserName, version: Version?): Result<Version> {
+            d { "Updating task list..." }
+            return netResult { tasksApi.getUpdates(version) }.mapCatching { changes -> storage.update(userName, changes) }
+        }
+
+        /**
          * Insert / update
          */
-        override fun upsertTask(userName: UserName, task: Task): Flow<LceState<Unit, AppError>> {
-            TODO("Not yet implemented")
+        override suspend fun upsertTask(userName: UserName, task: Task) {
+            d { "Upserting task: $task" }
+            updateServer(userName, listOf(TaskCommand.Upsert(task)))
         }
 
         /**
          * Delete task
          * @param taskId Task ID
          */
-        override fun deleteTask(userName: UserName, taskId: TaskId): Flow<LceState<Unit, AppError>> {
-            TODO("Not yet implemented")
+        override suspend fun deleteTask(userName: UserName, taskId: TaskId) {
+            d { "Deleting task: $taskId" }
+            updateServer(userName, listOf(TaskCommand.Delete(taskId)))
+        }
+
+        /**
+         * Runs update from server
+         * @param userName Bound user
+         * @param commands Commands to run
+         * @param currentVersion Current version override
+         */
+        private suspend fun updateServer(
+            userName: UserName,
+            commands: List<TaskCommand>,
+            currentVersion: Version? = null
+        ) {
+            // Loads data and retries the update
+            suspend fun syncAndRetry(version: Version?) {
+                updateServer(
+                    userName,
+                    commands,
+                    doLoad(userName, version).getOrThrow()
+                )
+            }
+
+            val effectiveCurrentVersion = currentVersion ?: storage.getVersion(userName).firstOrNull()
+            if (null == effectiveCurrentVersion) {
+                d { "Empty version. Updating data..." }
+                syncAndRetry(null)
+                return
+            }
+
+            val newVersion = nextVersion()
+            d { "Updating server" }
+            d { "Current version: $currentVersion" }
+            d { "New version: $newVersion" }
+
+            val update = netResult { tasksApi.postUpdates(TaskUpdateRequest(effectiveCurrentVersion, newVersion, commands)) }
+                .recover { error ->
+                    w(error) { "Error updating tasks" }
+                    if (error is AppError.WorkFlow && ErrorCode.CONFLICT == error.code) {
+                        d { "Data conflict. Retrying after data sync" }
+                        syncAndRetry(effectiveCurrentVersion)
+                        return
+                    }
+                    throw error
+                }
+                .getOrThrow()
+
+            storage.update(userName, TaskUpdates(update.latestVersion, commands))
         }
     }
 }

tasks/src/test/kotlin/com/motorro/tasks/app/repository/MemoryTasksRepositoryTest.kt
@@ -12,14 +12,19 @@ import com.motorro.tasks.data.ErrorCode
 import com.motorro.tasks.data.HttpResponse
 import com.motorro.tasks.data.Task
 import com.motorro.tasks.data.TaskCommand
+import com.motorro.tasks.data.TaskUpdateRequest
 import com.motorro.tasks.data.TaskUpdates
 import com.motorro.tasks.data.Version
+import com.motorro.tasks.data.VersionResponse
+import io.mockk.Ordering
 import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.cancelAndJoin
+import kotlinx.coroutines.flow.take
 import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.joinAll
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.runTest
@@ -28,6 +33,7 @@ import java.util.LinkedList
 import kotlin.test.assertContentEquals
 import kotlin.test.assertEquals
 import kotlin.test.assertIs
+import kotlin.test.assertNotEquals
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class MemoryTasksRepositoryTest {
@@ -114,6 +120,7 @@ class MemoryTasksRepositoryTest {
             tasks.map { it.toList() }
         )
         vCollector.cancelAndJoin()
+        @Suppress("RemoveExplicitTypeArguments")
         assertEquals<List<Version?>>(
             listOf(null),
             versions
@@ -121,6 +128,112 @@ class MemoryTasksRepositoryTest {
 
         coVerify {
             api.getUpdates(null)
+        }
+    }
+
+    @Test
+    fun upsertsTask() = runTest(dispatcher) {
+        val repository = createRepository()
+        val ver1 = Version("ver1")
+        coEvery { api.getUpdates(anyNullable()) } returns HttpResponse.Data(
+            TaskUpdates(
+                ver1,
+                listOf(task1).map(TaskCommand::Upsert)
+            )
+        )
+        coEvery { api.postUpdates(any()) } answers {
+            HttpResponse.Data(VersionResponse((args.first() as TaskUpdateRequest).nextVersion))
+        }
+
+        val tasks = LinkedList<Collection<Task>>()
+        val tCollector = launch {
+            repository.getTasks(USER_NAME).take(3).collect(tasks::add)
+        }
+
+        val versions = LinkedList<Version?>()
+        val vCollector = launch {
+            repository.getVersion(USER_NAME).take(3).collect(versions::add)
+        }
+
+        repository.upsertTask(USER_NAME, task2)
+
+        joinAll(tCollector, vCollector)
+        assertEquals(
+            listOf(emptyList(), listOf(task1), listOf(task1, task2)),
+            tasks.map { it.toList() }
+        )
+        assertEquals(3, versions.size)
+
+        coVerify {
+            api.getUpdates(null)
+            api.postUpdates(withArg {
+                assertEquals(ver1, it.expectedVersion)
+                assertNotEquals(ver1, it.nextVersion)
+                assertEquals(listOf(TaskCommand.Upsert(task2)), it.commands)
+            })
+        }
+    }
+
+    @Test
+    fun syncsAndRetriesOnVersionConflict() = runTest(dispatcher) {
+        val repository = createRepository()
+        val ver1 = Version("ver1")
+        val ver2 = Version("ver2")
+        coEvery { api.getUpdates(anyNullable()) }.returnsMany(
+            HttpResponse.Data(
+                TaskUpdates(
+                    ver1,
+                    listOf(task1).map(TaskCommand::Upsert)
+                )
+            ),
+            HttpResponse.Data(
+                TaskUpdates(
+                    ver2,
+                    listOf(task1.id).map(TaskCommand::Delete)
+                )
+            )
+        )
+        var index = 1
+        coEvery { api.postUpdates(any()) } answers {
+            when(index++) {
+                1 -> HttpResponse.Error(ErrorCode.CONFLICT, "Version conflict")
+                2 -> HttpResponse.Data(VersionResponse((args.first() as TaskUpdateRequest).nextVersion))
+                else -> throw IllegalStateException("Unexpected invocation")
+            }
+        }
+
+        val tasks = LinkedList<Collection<Task>>()
+        val tCollector = launch {
+            repository.getTasks(USER_NAME).take(4).collect(tasks::add)
+        }
+
+        val versions = LinkedList<Version?>()
+        val vCollector = launch {
+            repository.getVersion(USER_NAME).take(4).collect(versions::add)
+        }
+
+        repository.upsertTask(USER_NAME, task2)
+
+        joinAll(tCollector, vCollector)
+        assertEquals(
+            listOf(emptyList(), listOf(task1), emptyList(), listOf(task2)),
+            tasks.map { it.toList() }
+        )
+        assertEquals(4, versions.size)
+
+        coVerify(ordering = Ordering.SEQUENCE) {
+            api.getUpdates(null)
+            api.postUpdates(withArg {
+                assertEquals(ver1, it.expectedVersion)
+                assertNotEquals(ver1, it.nextVersion)
+                assertEquals(listOf(TaskCommand.Upsert(task2)), it.commands)
+            })
+            api.getUpdates(ver1)
+            api.postUpdates(withArg {
+                assertEquals(ver2, it.expectedVersion)
+                assertNotEquals(ver1, it.nextVersion)
+                assertEquals(listOf(TaskCommand.Upsert(task2)), it.commands)
+            })
         }
     }
 }