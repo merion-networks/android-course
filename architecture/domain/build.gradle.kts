motorro/Android

Domain session and profile Refs: #29

architecture/domain/build.gradle.kts
@@ -1,0 +1,29 @@
+plugins {
+    alias(libs.plugins.jetbrains.kotlin.jvm)
+    alias(libs.plugins.kotlin.serialization)
+}
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+kotlin {
+    jvmToolchain(17)
+}
+
+dependencies {
+    implementation(project(":architecture:core"))
+    implementation(project(":architecture:model"))
+
+    testImplementation(project(":architecture:domaintest"))
+    testImplementation(libs.junit)
+    testImplementation(libs.kotlin.test)
+    testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.mockk.mockk)
+}
+
+kotlin {
+    compilerOptions.optIn.add("kotlin.uuid.ExperimentalUuidApi")
+}
+
+
+

merion-networks/android-course

architecture/domain/build.gradle.kts
@@ -1,0 +1,29 @@
+plugins {
+    alias(libs.plugins.jetbrains.kotlin.jvm)
+    alias(libs.plugins.kotlin.serialization)
+}
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+kotlin {
+    jvmToolchain(21)
+}
+
+dependencies {
+    implementation(project(":architecture:core"))
+    implementation(project(":architecture:model"))
+
+    testImplementation(project(":architecture:domaintest"))
+    testImplementation(libs.junit)
+    testImplementation(libs.kotlin.test)
+    testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.mockk.mockk)
+}
+
+kotlin {
+    compilerOptions.optIn.add("kotlin.uuid.ExperimentalUuidApi")
+}
+
+
+