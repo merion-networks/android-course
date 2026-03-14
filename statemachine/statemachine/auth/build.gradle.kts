motorro/Android

Modularized auth, login, registration Refs: #34

statemachine/statemachine/auth/build.gradle.kts
@@ -1,0 +1,56 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatformAndroidLibrary)
+    alias(libs.plugins.compose)
+    alias(libs.plugins.composeMultiplatform)
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.composeHotReload)
+}
+
+kotlin {
+    jvm()
+
+    @Suppress("UnstableApiUsage")
+    androidLibrary {
+        namespace = "com.motorro.statemachine.auth"
+        compileSdk = libs.versions.compileSdk.get().toInt()
+        minSdk = libs.versions.minSdk.get().toInt()
+
+        androidResources.enable = true
+        compilerOptions {
+            jvmTarget = JvmTarget.JVM_17
+        }
+    }
+
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach { iosTarget ->
+        iosTarget.binaries.framework {
+            baseName = "auth"
+            isStatic = true
+        }
+    }
+
+    sourceSets {
+        commonMain {
+            dependencies {
+                implementation(project(":statemachine:common"))
+                implementation(project(":statemachine:statemachine:login"))
+                implementation(project(":statemachine:statemachine:register"))
+                implementation(libs.commonstatemachine.machine)
+                implementation(libs.commonstatemachine.coroutines)
+                implementation(compose.components.uiToolingPreview)
+            }
+        }
+    }
+}
+
+compose.resources {
+    publicResClass = false
+    packageOfResClass = "com.motorro.statemachine.auth"
+    generateResClass = always
+}
+

merion-networks/android-course

statemachine/statemachine/auth/build.gradle.kts
@@ -1,0 +1,56 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatformAndroidLibrary)
+    alias(libs.plugins.compose)
+    alias(libs.plugins.composeMultiplatform)
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.composeHotReload)
+}
+
+kotlin {
+    jvm()
+
+    @Suppress("UnstableApiUsage")
+    android {
+        namespace = "com.motorro.statemachine.auth"
+        compileSdk = libs.versions.compileSdk.get().toInt()
+        minSdk = libs.versions.minSdk.get().toInt()
+
+        androidResources.enable = true
+        compilerOptions {
+            jvmTarget = JvmTarget.JVM_21
+        }
+    }
+
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach { iosTarget ->
+        iosTarget.binaries.framework {
+            baseName = "auth"
+            isStatic = true
+        }
+    }
+
+    sourceSets {
+        commonMain {
+            dependencies {
+                implementation(project(":statemachine:common"))
+                implementation(project(":statemachine:statemachine:login"))
+                implementation(project(":statemachine:statemachine:register"))
+                implementation(libs.commonstatemachine.machine)
+                implementation(libs.commonstatemachine.coroutines)
+                implementation(libs.compose.multiplatform.uiToolingPreview)
+            }
+        }
+    }
+}
+
+compose.resources {
+    publicResClass = false
+    packageOfResClass = "com.motorro.statemachine.auth"
+    generateResClass = always
+}
+