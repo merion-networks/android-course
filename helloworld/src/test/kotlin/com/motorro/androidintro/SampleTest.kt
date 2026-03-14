motorro/Android

Sample test Refs: #1

helloworld/src/test/kotlin/ru/merionet/androidintro/SampleTest.kt
@@ -1,0 +1,14 @@
+package ru.merionet.androidintro
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class SampleTest {
+    @Test
+    fun twoPlusTwoEqualsFour() {
+        assertEquals(
+            4,
+            2 + 2
+        )
+    }
+}

merion-networks/android-course

helloworld/src/test/kotlin/com/motorro/androidintro/SampleTest.kt
@@ -1,0 +1,14 @@
+package com.motorro.androidintro
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class SampleTest {
+    @Test
+    fun twoPlusTwoEqualsFour() {
+        assertEquals(
+            4,
+            2 + 2
+        )
+    }
+}