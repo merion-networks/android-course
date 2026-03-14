motorro/Android

Saving tasks on server Refs: #4

tasks/src/main/kotlin/ru/merionet/tasks/app/state/AppStateFactory.kt
@@ -1,7 +1,9 @@
 package ru.merionet.tasks.app.state
 
 import ru.merionet.tasks.app.data.AppData
+import ru.merionet.tasks.app.data.AppError
 import ru.merionet.tasks.data.Task
+import ru.merionet.tasks.data.TaskId
 import ru.merionet.tasks.data.UserName
 import javax.inject.Inject
 
@@ -52,6 +54,21 @@ interface AppStateFactory {
     fun savingTask(data: AppData, task: Task): AppState
 
     /**
+     * Saving task error
+     */
+    fun savingTaskError(data: AppData, task: Task, error: AppError): AppState
+
+    /**
+     * Deleting task
+     */
+    fun deletingTask(data: AppData, taskId: TaskId): AppState
+
+    /**
+     * Deleting task error
+     */
+    fun deletingTaskError(data: AppData, taskId: TaskId, error: AppError): AppState
+
+    /**
      * Application terminated
      */
     fun terminated(): AppState
@@ -64,7 +81,8 @@ interface AppStateFactory {
         private val createLogin: LoginProxy.Factory,
         private val createLoggingOut: LoggingOutState.Factory,
         private val createTaskList: TaskListState.Factory,
-        private val createTask: TaskState.Factory
+        private val createTask: TaskState.Factory,
+        private val updateTask: UpdatingTaskState.Factory
     ) : AppStateFactory {
         private val context = object : AppContext {
             override val factory: AppStateFactory = this@Impl
@@ -112,9 +130,31 @@ interface AppStateFactory {
             task
         )
 
-        override fun savingTask(data: AppData, task: Task): AppState {
-            TODO("Not yet implemented")
-        }
+        override fun savingTask(data: AppData, task: Task) = updateTask.upsert(
+            context,
+            data,
+            task
+        )
+
+        override fun savingTaskError(data: AppData, task: Task, error: AppError) = ErrorState(
+            context,
+            error,
+            onRetry = { context.factory.savingTask(data, task) },
+            onBack = { context.factory.task(data, task) }
+        )
+
+        override fun deletingTask(data: AppData, taskId: TaskId) = updateTask.delete(
+            context,
+            data,
+            taskId
+        )
+
+        override fun deletingTaskError(data: AppData, taskId: TaskId, error: AppError) = ErrorState(
+            context,
+            error,
+            onRetry = { context.factory.deletingTask(data, taskId) },
+            onBack = { context.factory.taskList(data) }
+        )
 
         override fun terminated(): AppState = Terminated(context)
     }

tasks/src/main/kotlin/ru/merionet/tasks/app/state/UpdatingTaskState.kt
@@ -1,0 +1,113 @@
+package ru.merionet.tasks.app.state
+
+import kotlinx.coroutines.launch
+import ru.merionet.tasks.app.data.AppData
+import ru.merionet.tasks.app.data.AppError
+import ru.merionet.tasks.app.data.AppUiState
+import ru.merionet.tasks.app.data.toApp
+import ru.merionet.tasks.app.repository.TasksRepository
+import ru.merionet.tasks.data.Task
+import ru.merionet.tasks.data.TaskId
+import javax.inject.Inject
+
+/**
+ * Saves task to repository
+ */
+abstract class UpdatingTaskState(
+    context: AppContext,
+    protected val data: AppData,
+    private val tasksRepository: TasksRepository
+) : BaseAppState(context) {
+    /**
+     * A part of [start] template to initialize state
+     */
+    override fun doStart() {
+        super.doStart()
+        setUiState(AppUiState.Loading())
+        update()
+    }
+
+    /**
+     * Updates task data
+     */
+    abstract suspend fun TasksRepository.runUpdate()
+
+    /**
+     * Error handler
+     */
+    abstract fun onError(error: AppError)
+
+    /**
+     * Updates the repository
+     */
+    private fun update() {
+        d { "Updating task..." }
+        stateScope.launch {
+            try {
+                tasksRepository.runUpdate()
+                setMachineState(factory.taskList(data))
+            } catch (e: Throwable) {
+                w(e) { "Error updating task" }
+                onError(e.toApp())
+            }
+        }
+    }
+
+    /**
+     * State factory to inject all the external dependencies and keep the main factory
+     * clean
+     */
+    class Factory @Inject constructor(private val tasksRepository: TasksRepository) {
+        fun upsert(context: AppContext, data: AppData, task: Task): AppState = UpsertTask(
+            context,
+            data,
+            task,
+            tasksRepository
+        )
+
+        fun delete(context: AppContext, data: AppData, taskId: TaskId): AppState = DeleteTask(
+            context,
+            data,
+            taskId,
+            tasksRepository
+        )
+    }
+}
+
+/**
+ * Upsert task
+ */
+class UpsertTask(context: AppContext, data: AppData, private val task: Task, tasksRepository: TasksRepository) : UpdatingTaskState(context, data, tasksRepository) {
+    /**
+     * Updates task data
+     */
+    override suspend fun TasksRepository.runUpdate() {
+        upsertTask(data.user.name, task)
+    }
+
+    /**
+     * Error handler
+     */
+    override fun onError(error: AppError) {
+        setMachineState(factory.savingTaskError(data, task, error))
+    }
+}
+
+/**
+ * Delete task
+ */
+class DeleteTask(context: AppContext, data: AppData, private val taskId: TaskId, tasksRepository: TasksRepository) : UpdatingTaskState(context, data, tasksRepository) {
+    /**
+     * Updates task data
+     */
+    override suspend fun TasksRepository.runUpdate() {
+        deleteTask(data.user.name, taskId)
+    }
+
+    /**
+     * Error handler
+     */
+    override fun onError(error: AppError) {
+        setMachineState(factory.deletingTaskError(data, taskId, error))
+    }
+}

merion-networks/android-course

(cherry picked from commit 71c80dbba29b6f095f7247dafe106c4ef8d27002)

tasks/src/main/kotlin/com/motorro/tasks/app/state/AppStateFactory.kt
@@ -1,7 +1,9 @@
 package com.motorro.tasks.app.state
 
 import com.motorro.tasks.app.data.AppData
+import com.motorro.tasks.app.data.AppError
 import com.motorro.tasks.data.Task
+import com.motorro.tasks.data.TaskId
 import com.motorro.tasks.data.UserName
 import javax.inject.Inject
 
@@ -52,6 +54,21 @@ interface AppStateFactory {
     fun savingTask(data: AppData, task: Task): AppState
 
     /**
+     * Saving task error
+     */
+    fun savingTaskError(data: AppData, task: Task, error: AppError): AppState
+
+    /**
+     * Deleting task
+     */
+    fun deletingTask(data: AppData, taskId: TaskId): AppState
+
+    /**
+     * Deleting task error
+     */
+    fun deletingTaskError(data: AppData, taskId: TaskId, error: AppError): AppState
+
+    /**
      * Application terminated
      */
     fun terminated(): AppState
@@ -64,7 +81,8 @@ interface AppStateFactory {
         private val createLogin: LoginProxy.Factory,
         private val createLoggingOut: LoggingOutState.Factory,
         private val createTaskList: TaskListState.Factory,
-        private val createTask: TaskState.Factory
+        private val createTask: TaskState.Factory,
+        private val updateTask: UpdatingTaskState.Factory
     ) : AppStateFactory {
         private val context = object : AppContext {
             override val factory: AppStateFactory = this@Impl
@@ -112,9 +130,31 @@ interface AppStateFactory {
             task
         )
 
-        override fun savingTask(data: AppData, task: Task): AppState {
-            TODO("Not yet implemented")
-        }
+        override fun savingTask(data: AppData, task: Task) = updateTask.upsert(
+            context,
+            data,
+            task
+        )
+
+        override fun savingTaskError(data: AppData, task: Task, error: AppError) = ErrorState(
+            context,
+            error,
+            onRetry = { context.factory.savingTask(data, task) },
+            onBack = { context.factory.task(data, task) }
+        )
+
+        override fun deletingTask(data: AppData, taskId: TaskId) = updateTask.delete(
+            context,
+            data,
+            taskId
+        )
+
+        override fun deletingTaskError(data: AppData, taskId: TaskId, error: AppError) = ErrorState(
+            context,
+            error,
+            onRetry = { context.factory.deletingTask(data, taskId) },
+            onBack = { context.factory.taskList(data) }
+        )
 
         override fun terminated(): AppState = Terminated(context)
     }

tasks/src/main/kotlin/com/motorro/tasks/app/state/UpdatingTaskState.kt
@@ -1,0 +1,113 @@
+package com.motorro.tasks.app.state
+
+import com.motorro.tasks.app.data.AppData
+import com.motorro.tasks.app.data.AppError
+import com.motorro.tasks.app.data.AppUiState
+import com.motorro.tasks.app.data.toApp
+import com.motorro.tasks.app.repository.TasksRepository
+import com.motorro.tasks.data.Task
+import com.motorro.tasks.data.TaskId
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+/**
+ * Saves task to repository
+ */
+abstract class UpdatingTaskState(
+    context: AppContext,
+    protected val data: AppData,
+    private val tasksRepository: TasksRepository
+) : BaseAppState(context) {
+    /**
+     * A part of [start] template to initialize state
+     */
+    override fun doStart() {
+        super.doStart()
+        setUiState(AppUiState.Loading())
+        update()
+    }
+
+    /**
+     * Updates task data
+     */
+    abstract suspend fun TasksRepository.runUpdate()
+
+    /**
+     * Error handler
+     */
+    abstract fun onError(error: AppError)
+
+    /**
+     * Updates the repository
+     */
+    private fun update() {
+        d { "Updating task..." }
+        stateScope.launch {
+            try {
+                tasksRepository.runUpdate()
+                setMachineState(factory.taskList(data))
+            } catch (e: Throwable) {
+                w(e) { "Error updating task" }
+                onError(e.toApp())
+            }
+        }
+    }
+
+    /**
+     * State factory to inject all the external dependencies and keep the main factory
+     * clean
+     */
+    class Factory @Inject constructor(private val tasksRepository: TasksRepository) {
+        fun upsert(context: AppContext, data: AppData, task: Task): AppState = UpsertTask(
+            context,
+            data,
+            task,
+            tasksRepository
+        )
+
+        fun delete(context: AppContext, data: AppData, taskId: TaskId): AppState = DeleteTask(
+            context,
+            data,
+            taskId,
+            tasksRepository
+        )
+    }
+}
+
+/**
+ * Upsert task
+ */
+class UpsertTask(context: AppContext, data: AppData, private val task: Task, tasksRepository: TasksRepository) : UpdatingTaskState(context, data, tasksRepository) {
+    /**
+     * Updates task data
+     */
+    override suspend fun TasksRepository.runUpdate() {
+        upsertTask(data.user.name, task)
+    }
+
+    /**
+     * Error handler
+     */
+    override fun onError(error: AppError) {
+        setMachineState(factory.savingTaskError(data, task, error))
+    }
+}
+
+/**
+ * Delete task
+ */
+class DeleteTask(context: AppContext, data: AppData, private val taskId: TaskId, tasksRepository: TasksRepository) : UpdatingTaskState(context, data, tasksRepository) {
+    /**
+     * Updates task data
+     */
+    override suspend fun TasksRepository.runUpdate() {
+        deleteTask(data.user.name, taskId)
+    }
+
+    /**
+     * Error handler
+     */
+    override fun onError(error: AppError) {
+        setMachineState(factory.deletingTaskError(data, taskId, error))
+    }
+}