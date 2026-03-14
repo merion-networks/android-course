motorro/Android

Recipe demo application Refs: #31

cookbook/recipe/recipedemo/build.gradle.kts
@@ -1,0 +1,71 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.navigation.safeargs.kotlin)
+    alias(libs.plugins.google.ksp)
+    alias(libs.plugins.hilt)
+}
+
+android {
+    namespace = "com.motorro.cookbook.recipedemo"
+    compileSdk = libs.versions.compileSdk.get().toInt()
+
+    defaultConfig {
+        applicationId = "com.motorro.cookbook.recipedemo"
+        minSdk = libs.versions.minSdk.get().toInt()
+        targetSdk = libs.versions.targetSdk.get().toInt()
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlin {
+        compilerOptions {
+            jvmTarget = JvmTarget.JVM_17
+        }
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    implementation(project(":cookbook:domain"))
+    implementation(project(":cookbook:appcore"))
+    implementation(project(":cookbook:recipe"))
+    implementation(project(":cookbook:mockdata"))
+
+    implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+
+    implementation(libs.hilt.android)
+    implementation(libs.hilt.androidx)
+    ksp(libs.hilt.android.compiler)
+}
+
+kotlin {
+    compilerOptions.optIn.add("kotlin.uuid.ExperimentalUuidApi")
+}
+
+configurations.implementation{
+    exclude(group = "com.intellij", module = "annotations")
+}

merion-networks/android-course

cookbook/recipe/recipedemo/build.gradle.kts
@@ -1,0 +1,70 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.navigation.safeargs.kotlin)
+    alias(libs.plugins.google.ksp)
+    alias(libs.plugins.hilt)
+}
+
+android {
+    namespace = "com.motorro.cookbook.recipedemo"
+    compileSdk = libs.versions.compileSdk.get().toInt()
+
+    defaultConfig {
+        applicationId = "com.motorro.cookbook.recipedemo"
+        minSdk = libs.versions.minSdk.get().toInt()
+        targetSdk = libs.versions.targetSdk.get().toInt()
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+    kotlin {
+        compilerOptions {
+            jvmTarget = JvmTarget.JVM_21
+        }
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    implementation(project(":cookbook:domain"))
+    implementation(project(":cookbook:appcore"))
+    implementation(project(":cookbook:recipe"))
+    implementation(project(":cookbook:mockdata"))
+
+    implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+
+    implementation(libs.hilt.android)
+    implementation(libs.hilt.androidx)
+    ksp(libs.hilt.android.compiler)
+}
+
+kotlin {
+    compilerOptions.optIn.add("kotlin.uuid.ExperimentalUuidApi")
+}
+
+configurations.implementation{
+    exclude(group = "com.intellij", module = "annotations")
+}