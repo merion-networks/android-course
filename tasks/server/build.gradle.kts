motorro/Android

Login data and server Refs: #4

tasks/server/build.gradle.kts
@@ -1,0 +1,37 @@
+plugins {
+    alias(libs.plugins.jetbrains.kotlin.jvm)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.ktor)
+    application
+}
+
+group = "com.motorro.tasks"
+version = "1.0.0"
+application {
+    mainClass.set("com.motorro.tasks.server.ApplicationKt")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=${extra["io.ktor.development"] ?: "false"}")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+dependencies {
+    implementation(project(":tasks:data"))
+    implementation(libs.logback)
+    implementation(libs.ktor.server.core)
+    implementation(libs.ktor.server.netty)
+    implementation(libs.ktor.server.netty)
+    implementation(libs.ktor.server.status)
+    implementation(libs.ktor.server.contentType)
+    implementation(libs.ktor.serialization.contentJson)
+    implementation(libs.ktor.server.auth)
+    testImplementation(libs.ktor.server.tests)
+    testImplementation(libs.kotlin.test)
+    testImplementation(libs.junit)
+}

merion-networks/android-course

tasks/server/build.gradle.kts
@@ -1,0 +1,37 @@
+plugins {
+    alias(libs.plugins.jetbrains.kotlin.jvm)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.ktor)
+    application
+}
+
+group = "com.motorro.tasks"
+version = "1.0.0"
+application {
+    mainClass.set("com.motorro.tasks.server.ApplicationKt")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=${extra["io.ktor.development"] ?: "false"}")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+dependencies {
+    implementation(project(":tasks:data"))
+    implementation(libs.logback)
+    implementation(libs.ktor.server.core)
+    implementation(libs.ktor.server.netty)
+    implementation(libs.ktor.server.netty)
+    implementation(libs.ktor.server.status)
+    implementation(libs.ktor.server.contentType)
+    implementation(libs.ktor.serialization.contentJson)
+    implementation(libs.ktor.server.auth)
+    testImplementation(libs.ktor.server.tests)
+    testImplementation(libs.kotlin.test)
+    testImplementation(libs.junit)
+}