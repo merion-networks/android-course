motorro/Android

Login form integration Refs: #4

tasks/src/main/kotlin/ru/merionet/tasks/MainActivityViewModel.kt
@@ -1,0 +1,32 @@
+package ru.merionet.tasks
+
+import androidx.lifecycle.ViewModel
+import com.motorro.commonstatemachine.coroutines.FlowStateMachine
+import kotlinx.coroutines.flow.StateFlow
+import ru.merionet.tasks.login.data.LoginGesture
+import ru.merionet.tasks.login.data.LoginUiState
+import ru.merionet.tasks.login.state.LoginStateFactory
+
+/**
+ * Common view-model for the app
+ */
+class MainActivityViewModel : ViewModel() {
+    /**
+     * Common state machine
+     */
+    private val stateMachine = FlowStateMachine(LoginUiState.Loading) {
+        LoginStateFactory.Impl().init()
+    }
+
+    /**
+     * UI state export
+     */
+    val uiState: StateFlow<LoginUiState> get() = stateMachine.uiState
+
+    /**
+     * Gesture processing
+     */
+    fun process(gesture: LoginGesture) {
+        stateMachine.process(gesture)
+    }
+}

tasks/src/main/kotlin/ru/merionet/tasks/MainScreen.kt
@@ -1,0 +1,24 @@
+package ru.merionet.tasks
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import ru.merionet.tasks.login.data.LoginGesture
+import ru.merionet.tasks.login.data.LoginUiState
+import ru.merionet.tasks.login.view.LoginScreen
+import ru.merionet.tasks.ui.LoadingScreen
+import ru.merionet.tasks.ui.theme.AppTheme
+
+@Composable
+fun MainScreen(state: LoginUiState, onComplete: () -> Unit, onGesture: (LoginGesture) -> Unit) {
+    AppTheme {
+        when(state) {
+            is LoginUiState.Loading -> LoadingScreen {
+                onGesture(LoginGesture.Back)
+            }
+            is LoginUiState.Form -> LoginScreen(state, onGesture)
+            LoginUiState.Terminated -> LaunchedEffect(Unit) {
+                onComplete()
+            }
+        }
+    }
+}

tasks/src/main/kotlin/ru/merionet/tasks/login/state/LoggingInState.kt
@@ -1,0 +1,31 @@
+package ru.merionet.tasks.login.state
+
+import ru.merionet.tasks.login.data.LoginData
+import ru.merionet.tasks.login.data.LoginGesture
+import ru.merionet.tasks.login.data.LoginUiState
+
+/**
+ * Runs login operation on server
+ */
+class LoggingInState(context: LoginContext, private val data: LoginData) : BaseLoginState(context) {
+    /**
+     * A part of [start] template to initialize state
+     */
+    override fun doStart() {
+        super.doStart()
+        setUiState(LoginUiState.Loading)
+    }
+
+    /**
+     * A part of [process] template to process UI gesture
+     */
+    override fun doProcess(gesture: LoginGesture) {
+        when(gesture) {
+            LoginGesture.Back -> {
+                d { "Back pressed. Returning to login form..." }
+                setMachineState(factory.form(data))
+            }
+            else -> super.doProcess(gesture)
+        }
+    }
+}

tasks/src/main/kotlin/ru/merionet/tasks/login/state/TerminatedState.kt
@@ -1,0 +1,13 @@
+package ru.merionet.tasks.login.state
+
+import ru.merionet.tasks.login.data.LoginUiState
+
+class TerminatedState(context: LoginContext) : BaseLoginState(context) {
+    /**
+     * A part of [start] template to initialize state
+     */
+    override fun doStart() {
+        super.doStart()
+        setUiState(LoginUiState.Terminated)
+    }
+}

tasks/src/main/kotlin/ru/merionet/tasks/login/view/LoginScreen.kt
@@ -1,0 +1,64 @@
+package ru.merionet.tasks.login.view
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import ru.merionet.tasks.R
+import ru.merionet.tasks.login.data.LoginGesture
+import ru.merionet.tasks.login.data.LoginUiState
+import ru.merionet.tasks.ui.ScreenScaffold
+
+@Composable
+fun LoginScreen(state: LoginUiState.Form, onGesture: (LoginGesture) -> Unit) {
+    ScreenScaffold(
+        title = stringResource(R.string.title_login),
+        onBack = { onGesture(LoginGesture.Back) },
+        content = { padding ->
+            Column(
+                modifier = Modifier.fillMaxSize().padding(padding),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    modifier = Modifier.padding(16.dp),
+                    text = stringResource(R.string.title_login),
+                    style = MaterialTheme.typography.titleLarge
+                )
+                TextField(
+                    modifier = Modifier.padding(16.dp),
+                    value = state.userName,
+                    onValueChange = { onGesture(LoginGesture.UserNameChanged(it)) },
+                    label = { Text(stringResource(R.string.hint_username)) }
+                )
+                TextField(
+                    modifier = Modifier.padding(16.dp),
+                    value = state.password,
+                    onValueChange = { onGesture(LoginGesture.PasswordChanged(it)) },
+                    label = { Text(stringResource(R.string.hint_password)) },
+                    visualTransformation = PasswordVisualTransformation(),
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                )
+                Button(
+                    modifier = Modifier.padding(16.dp),
+                    onClick = { onGesture(LoginGesture.Action) },
+                    enabled = state.loginEnabled
+                ) {
+                    Text(stringResource(R.string.btn_login))
+                }
+            }
+        }
+    )
+}

tasks/src/test/kotlin/ru/merionet/tasks/login/state/LoggingInStateTest.kt
@@ -1,0 +1,33 @@
+package ru.merionet.tasks.login.state
+
+import io.mockk.Ordering
+import io.mockk.every
+import io.mockk.verify
+import org.junit.Test
+import ru.merionet.tasks.login.data.LoginGesture
+import ru.merionet.tasks.login.data.LoginUiState
+
+internal class LoggingInStateTest : BaseStateTest() {
+    private val state: LoginState = LoggingInState(context, loginData)
+
+    @Test
+    fun rendersLoadingOnStart() {
+        state.start(stateMachine)
+
+        verify {
+            stateMachine.setUiState(LoginUiState.Loading)
+        }
+    }
+
+    @Test
+    fun returnsToFormOnBack() {
+        every { factory.form(any()) } returns nextState
+
+        state.start(stateMachine)
+        state.process(LoginGesture.Back)
+        verify(ordering = Ordering.ORDERED) {
+            factory.form(loginData)
+            stateMachine.setMachineState(nextState)
+        }
+    }
+}

merion-networks/android-course

(cherry picked from commit c46102aa10dbdfbb96d087b124c1a53ab1d3a1ad)

tasks/src/main/kotlin/com/motorro/tasks/MainActivityViewModel.kt
@@ -1,0 +1,32 @@
+package com.motorro.tasks
+
+import androidx.lifecycle.ViewModel
+import com.motorro.commonstatemachine.coroutines.FlowStateMachine
+import com.motorro.tasks.login.data.LoginGesture
+import com.motorro.tasks.login.data.LoginUiState
+import com.motorro.tasks.login.state.LoginStateFactory
+import kotlinx.coroutines.flow.StateFlow
+
+/**
+ * Common view-model for the app
+ */
+class MainActivityViewModel : ViewModel() {
+    /**
+     * Common state machine
+     */
+    private val stateMachine = FlowStateMachine(LoginUiState.Loading) {
+        LoginStateFactory.Impl().init()
+    }
+
+    /**
+     * UI state export
+     */
+    val uiState: StateFlow<LoginUiState> get() = stateMachine.uiState
+
+    /**
+     * Gesture processing
+     */
+    fun process(gesture: LoginGesture) {
+        stateMachine.process(gesture)
+    }
+}

tasks/src/main/kotlin/com/motorro/tasks/MainScreen.kt
@@ -1,0 +1,24 @@
+package com.motorro.tasks
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import com.motorro.composecore.ui.LoadingScreen
+import com.motorro.composecore.ui.theme.AppTheme
+import com.motorro.tasks.login.data.LoginGesture
+import com.motorro.tasks.login.data.LoginUiState
+import com.motorro.tasks.login.view.LoginScreen
+
+@Composable
+fun MainScreen(state: LoginUiState, onComplete: () -> Unit, onGesture: (LoginGesture) -> Unit) {
+    AppTheme {
+        when(state) {
+            is LoginUiState.Loading -> LoadingScreen {
+                onGesture(LoginGesture.Back)
+            }
+            is LoginUiState.Form -> LoginScreen(state, onGesture)
+            LoginUiState.Terminated -> LaunchedEffect(Unit) {
+                onComplete()
+            }
+        }
+    }
+}

tasks/src/main/kotlin/com/motorro/tasks/login/state/LoggingInState.kt
@@ -1,0 +1,31 @@
+package com.motorro.tasks.login.state
+
+import com.motorro.tasks.login.data.LoginData
+import com.motorro.tasks.login.data.LoginGesture
+import com.motorro.tasks.login.data.LoginUiState
+
+/**
+ * Runs login operation on server
+ */
+class LoggingInState(context: LoginContext, private val data: LoginData) : BaseLoginState(context) {
+    /**
+     * A part of [start] template to initialize state
+     */
+    override fun doStart() {
+        super.doStart()
+        setUiState(LoginUiState.Loading)
+    }
+
+    /**
+     * A part of [process] template to process UI gesture
+     */
+    override fun doProcess(gesture: LoginGesture) {
+        when(gesture) {
+            LoginGesture.Back -> {
+                d { "Back pressed. Returning to login form..." }
+                setMachineState(factory.form(data))
+            }
+            else -> super.doProcess(gesture)
+        }
+    }
+}

tasks/src/main/kotlin/com/motorro/tasks/login/state/TerminatedState.kt
@@ -1,0 +1,14 @@
+package com.motorro.tasks.login.state
+
+import com.motorro.tasks.login.data.LoginUiState
+
+
+class TerminatedState(context: LoginContext) : BaseLoginState(context) {
+    /**
+     * A part of [start] template to initialize state
+     */
+    override fun doStart() {
+        super.doStart()
+        setUiState(LoginUiState.Terminated)
+    }
+}

tasks/src/main/kotlin/com/motorro/tasks/login/view/LoginScreen.kt
@@ -1,0 +1,64 @@
+package com.motorro.tasks.login.view
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import com.motorro.composecore.ui.ScreenScaffold
+import com.motorro.tasks.R
+import com.motorro.tasks.login.data.LoginGesture
+import com.motorro.tasks.login.data.LoginUiState
+
+@Composable
+fun LoginScreen(state: LoginUiState.Form, onGesture: (LoginGesture) -> Unit) {
+    ScreenScaffold(
+        title = stringResource(R.string.title_login),
+        onBack = { onGesture(LoginGesture.Back) },
+        content = { padding ->
+            Column(
+                modifier = Modifier.fillMaxSize().padding(padding),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    modifier = Modifier.padding(16.dp),
+                    text = stringResource(R.string.title_login),
+                    style = MaterialTheme.typography.titleLarge
+                )
+                TextField(
+                    modifier = Modifier.padding(16.dp),
+                    value = state.userName,
+                    onValueChange = { onGesture(LoginGesture.UserNameChanged(it)) },
+                    label = { Text(stringResource(R.string.hint_username)) }
+                )
+                TextField(
+                    modifier = Modifier.padding(16.dp),
+                    value = state.password,
+                    onValueChange = { onGesture(LoginGesture.PasswordChanged(it)) },
+                    label = { Text(stringResource(R.string.hint_password)) },
+                    visualTransformation = PasswordVisualTransformation(),
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                )
+                Button(
+                    modifier = Modifier.padding(16.dp),
+                    onClick = { onGesture(LoginGesture.Action) },
+                    enabled = state.loginEnabled
+                ) {
+                    Text(stringResource(R.string.btn_login))
+                }
+            }
+        }
+    )
+}

tasks/src/test/kotlin/com/motorro/tasks/login/state/LoggingInStateTest.kt
@@ -1,0 +1,33 @@
+package com.motorro.tasks.login.state
+
+import com.motorro.tasks.login.data.LoginGesture
+import com.motorro.tasks.login.data.LoginUiState
+import io.mockk.Ordering
+import io.mockk.every
+import io.mockk.verify
+import org.junit.Test
+
+internal class LoggingInStateTest : BaseStateTest() {
+    private val state: LoginState = LoggingInState(context, loginData)
+
+    @Test
+    fun rendersLoadingOnStart() {
+        state.start(stateMachine)
+
+        verify {
+            stateMachine.setUiState(LoginUiState.Loading)
+        }
+    }
+
+    @Test
+    fun returnsToFormOnBack() {
+        every { factory.form(any()) } returns nextState
+
+        state.start(stateMachine)
+        state.process(LoginGesture.Back)
+        verify(ordering = Ordering.ORDERED) {
+            factory.form(loginData)
+            stateMachine.setMachineState(nextState)
+        }
+    }
+}