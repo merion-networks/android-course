motorro/Android

Server: user list and profile Refs: #26

network/server/build.gradle.kts
@@ -1,0 +1,41 @@
+plugins {
+    alias(libs.plugins.jetbrains.kotlin.jvm)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.ktor)
+    application
+}
+
+group = "com.motorro.network"
+version = "1.0.0"
+application {
+    mainClass.set("com.motorro.network.server.ApplicationKt")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=${extra["io.ktor.development"] ?: "false"}")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+dependencies {
+    implementation(project(":network:data"))
+    implementation(libs.logback)
+    implementation(libs.ktor.server.core)
+    implementation(libs.ktor.server.netty)
+    implementation(libs.ktor.server.netty)
+    implementation(libs.ktor.server.status)
+    implementation(libs.ktor.server.contentType)
+    implementation(libs.ktor.serialization.contentJson)
+    implementation(libs.ktor.server.auth)
+    implementation(libs.ktor.server.openapi)
+    testImplementation(libs.ktor.server.tests)
+    testImplementation(libs.ktor.client.core)
+    testImplementation(libs.ktor.client.content.negotiation)
+    testImplementation(libs.kotlin.test)
+    testImplementation(libs.junit)
+    testImplementation(libs.mockk.mockk)
+}

merion-networks/android-course

network/server/build.gradle.kts
@@ -1,0 +1,41 @@
+plugins {
+    alias(libs.plugins.jetbrains.kotlin.jvm)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.ktor)
+    application
+}
+
+group = "com.motorro.network"
+version = "1.0.0"
+application {
+    mainClass.set("com.motorro.network.server.ApplicationKt")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=${extra["io.ktor.development"] ?: "false"}")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+dependencies {
+    implementation(project(":network:data"))
+    implementation(libs.logback)
+    implementation(libs.ktor.server.core)
+    implementation(libs.ktor.server.netty)
+    implementation(libs.ktor.server.netty)
+    implementation(libs.ktor.server.status)
+    implementation(libs.ktor.server.contentType)
+    implementation(libs.ktor.serialization.contentJson)
+    implementation(libs.ktor.server.auth)
+    implementation(libs.ktor.server.openapi)
+    testImplementation(libs.ktor.server.tests)
+    testImplementation(libs.ktor.client.core)
+    testImplementation(libs.ktor.client.content.negotiation)
+    testImplementation(libs.kotlin.test)
+    testImplementation(libs.junit)
+    testImplementation(libs.mockk.mockk)
+}